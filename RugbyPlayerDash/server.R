f169e6de535f6decb7d62616c88b515b86f6aea3
Jack Royal <jackroyal1998@gmail.com>
2020-04-10 14:52:24 +1200

    Added a tab for plot which will look at the top 10 values for each team based off a certain attribute

--- RugbyPlayerDash/server.R
+++ RugbyPlayerDash/server.R
@@ -1,26 +1,34 @@
 library(shiny)
 library(DT)
 library(dplyr)
-
-#Interactive player data table - acts as a database of sorts
-
-
-rugby <- read.csv("RugbyPlayerDataESPN.csv")
+library(ggthemes)
+theme_set("Light")
+rugby <- read.csv("RugbyPlayerDataESPN1.csv")
 
 server <- function(input, output) {
+    
+    #Interactive player data table - acts as a database of sorts
     output$rugbydb <- renderDT({
         rdb <- rugby %>% arrange(-Mat)
         
         datatable(
                 rdb,
                 filter = "top",
-                class = "display nowrap compact",
+                class = "cell-border stripe",
                 options = list(scrollX = TRUE,
                                searching = TRUE)
             )
         
         
     })
+    output$plot1 <- renderPlot({
+        #could have like 4 plots each with a different attribute
+        Top10 <- rugby %>% filter(Team == "NZ") %>% top_n(number, Mat)
+        p <- ggplot(Top10, aes(x = reorder(Player,-Mat,sum),y = Mat)) + geom_bar(stat = "identity")
+        p + theme_wsj()+ scale_colour_wsj("colors6") +
+            ggtitle("Top 10 by Pts") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    })
+    
 }
 
 
--- RugbyPlayerDash/ui.R
+++ RugbyPlayerDash/ui.R
@@ -4,6 +4,14 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    h2("Player_DB"),
-    DT::dataTableOutput("rugbydb")
+  titlePanel("Rugby Analysis"),
+  mainPanel(
+    
+    # Output: Tabset w/ plot, summary, and table ----
+    tabsetPanel(type = "tabs",
+                tabPanel("Player Database", DT::dataTableOutput("rugbydb")),
+                tabPanel("Top 10 Plot", plotOutput("plot1"))
+                )
+    )
+    
 ))
